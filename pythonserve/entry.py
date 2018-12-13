5aa1477f1e3ab97c904d56b3b713e4f05fd611bb
catenocrypt <catenocrypt@users.noreply.github.com>
2018-12-13 10:08:10 +0100

    Dashboard minor changes.

--- pythonserve/entry.py
+++ pythonserve/entry.py
@@ -1,12 +1,15 @@
 from bottle import run, post, request, response, get, route, static_file
 
-@route('/explorer/<filename>', method='GET')
-def get_file(filename):
-    return static_file(filename, root="web/")
+#@route('/explorer/<filename>', method='GET')
+#def get_file(filename):
+#   return static_file(filename, root="web/")
 
+@route('/dashboard/', method='GET')
+@route('/dashboard', method='GET')
+# BW compat
 @route('/explorer/', method='GET')
 @route('/explorer', method='GET')
-def get_file_index():
-    return static_file("index.html", root="web/")
+def get_dashboard_index():
+    return static_file("dashboard.html", root="web/")
 
 run(host='localhost', port=8228, debug=True)
